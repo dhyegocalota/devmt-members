8890743968bc6e4ca480852c38d0bcb1d0722a44
dhyegofernando <dhyegofernando@gmail.com>
2019-03-09 02:36:05 -0400

    Add support for disabled on Button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,13 +1,22 @@
 import React, { PureComponent } from 'react';
+import cx from 'classnames';
 import './style.css';
 
 export default class Button extends PureComponent {
   static defaultProps = { variant: 'primary' };
 
   render() {
-    const { className, variant, children, ...props } = this.props;
+    const { className, variant, disabled, children, ...props } = this.props;
     return (
-      <a {...props} className={`Button Button--${variant} ${className}`}>
+      <a
+        {...props}
+        className={cx([
+          'Button',
+          `Button--${variant}`,
+          className,
+          { 'Button--disabled': disabled },
+        ])}
+      >
         {children}
       </a>
     );
